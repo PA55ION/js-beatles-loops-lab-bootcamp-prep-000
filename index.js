304bffade1088484773016eab2d842a22a0e4d7c
Mai Campbell <jirallc@gmail.com>
2018-07-19 15:48:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function theBeatlesPlay(musician, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for(let i = 0; i < musicians.length; i++) {
     arr.push(musicians[i] + ' play' + instruments[i]);
   }
-  return arr; 
+  return arr;
 }
